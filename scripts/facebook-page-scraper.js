8549c373e6952fdb72f6a28b2aaa1e7f430e9d95
DuongDN <duongdn@nustechnology.com>
2026-07-02 09:44:22 +0700

    auto: 2026-07-02 09:44

--- scripts/facebook-page-scraper.js
+++ scripts/facebook-page-scraper.js
@@ -15,7 +15,9 @@
  * Errors/progress printed to stderr.
  */
 
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 const fs = require('fs');
 const path = require('path');
 
